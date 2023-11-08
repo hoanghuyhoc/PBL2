645e54b6851eaaf0188b90c6264ecb6798271699
hungle <133577937+kophaihungle@users.noreply.github.com>
2023-11-08 08:35:20 +0700

    add notes folder

--- PlayerClassWarrior.cpp
+++ PlayerClassWarrior.cpp
@@ -16,5 +16,6 @@ void Warrior::Show_Description()
 }
 void Warrior::Show_Skill_Description()
 {
-
+        cout << "   WARRIOR'S ULTIMATE [O O O]" << endl;
+        cout << "   Deals 200% of ATK as damage to the target and\n   gain a 50% ATK Boost on the next 2 turns.";
 }
