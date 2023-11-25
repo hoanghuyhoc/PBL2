9006b4c0994251ea4ea58a9e127fef5b698f37ab
kophaihungle <hunggonduty234212@gmail.com>
2023-11-25 14:37:19 +0700

    changes Weapon.h, added Weapon.cpp with read file function (not finalised) changes Player.cpp (only 1 bug in Player.cpp)

--- Player.h
+++ Player.h
@@ -8,18 +8,20 @@ class Player: protected Character
 {
     private:
         double XP;
-        Weapon *Player_Weapon;
+        Weapon *equippedWeapon;
         Item *Player_Item;
         long Money;
         int SP;
     public:
         Player(std::string, double, double, double);
+        Player(Weapon* weapon);
         void Show_Status();
         void Attack(Enemy&);
-        virtual void Skill()=0;
+        void Skill();
         void Using_Item();
         void Show_Description();
         void Show_Skill_Description();
         void Gain_XP();
+        Weapon* getEquippedWeapon();
 };
 #endif
