3723ae763798958187f8cba54265044234d79d3e
Hoàng Huy Học <123890623+hoanghuyhoc@users.noreply.github.com>
2023-12-27 17:28:57 +0700

    Khởi tạo dsach Area từ file txt và chạy từng Area

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <fstream>
+#include <sstream>
 #include "Character.h"
 #include "Player.h"
 #include "Enemy.h"
@@ -23,8 +25,44 @@ int main()
         if (Menu::PlayStatus==2) break;
         Player *mainplayer=nullptr;
         Menu::StartGame(mainplayer);
-        Area Area1("Area1",3,1,nullptr);
-        Area1.EnterArea(mainplayer);
+
+        //khởi tạo danh sách Area từ file txt
+        std::fstream AreaFileList;
+        AreaFileList.open("Area.txt");
+        if (!AreaFileList.is_open())
+        {
+            std::cerr<<"Cannot open data file!\n";
+            system("pause");
+            return 1;
+        }
+        std::string line;
+        Area* AreaList=nullptr;
+        while (std::getline(AreaFileList,line))
+        {
+            std::istringstream input(line);
+            std::string name; 
+            int nodenumber, level;
+            input>>name>>nodenumber>>level;
+            Area* temp=new Area(name,nodenumber,level,nullptr);
+            temp->InsertIntoAreaList(AreaList);
+        }
+
+        //Chạy từng Area
+        while (AreaList!=nullptr)
+        {
+            AreaList->EnterArea(mainplayer);
+            AreaList=AreaList->EnterNextArea();
+        }
+        //destructor
+            //delete AreaList
+                while(AreaList!=nullptr)
+                {
+                    Area* temp=AreaList;
+                    AreaList=AreaList->EnterNextArea();
+                    delete temp;
+                }
+        //delete mainplayer;
+        delete mainplayer;
     } while (Menu::PlayStatus!=2);
     return 0;
 }
